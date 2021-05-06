a9636e67ed40ddea92013d8615fe6e7c5f722978
김건우 <kw.kim@xenoimpact.com>
2021-05-06 21:24:41 +0900

    [Add] - Show value types size

--- src/chap01/Test.java
+++ src/chap01/Test.java
@@ -2,6 +2,9 @@ package chap01;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        byte byteValue = 10;
+        System.out.println(byteValue);
+        System.out.println(Byte.BYTES);
     }
 }
