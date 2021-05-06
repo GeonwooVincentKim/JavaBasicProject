f727213f65c4a83804b94ad5471ef43c14f4f78c
김건우 <kw.kim@xenoimpact.com>
2021-05-06 21:29:28 +0900

    [Add] - Added data-type-conversion

--- src/chap01/Test.java
+++ src/chap01/Test.java
@@ -2,9 +2,28 @@ package chap01;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
+        // byte byteValue = 10;
+        // System.out.println(byteValue);
+        // System.out.println(Byte.BYTES);
+
         byte byteValue = 10;
-        System.out.println(byteValue);
-        System.out.println(Byte.BYTES);
+        int intValue = byteValue;
+        System.out.println("intValue : " + intValue);
+
+        char charValue = '가';
+        intValue = charValue;
+        System.out.println("가의 유니코드 : " + intValue);
+
+        intValue = 50;
+        long longValue = intValue;
+        System.out.println("longValue : " + longValue);
+
+        longValue = 100;
+        float floatValue = longValue;
+        System.out.println("floatValue : " + floatValue);
+
+        floatValue = 100.5F;
+        double doubleValue = floatValue;
+        System.out.println("doubleValue : " + doubleValue);
     }
 }
