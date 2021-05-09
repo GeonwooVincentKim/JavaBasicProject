32cd96cfef4d8a08a2871de6be321c33ceee1851
김건우 <kw.kim@xenoimpact.com>
2021-05-09 15:34:47 +0900

    [Feature] - Complete to implement Auto-CastingTypeValue

--- src/chap01/AutoCastingExample.java
+++ src/chap01/AutoCastingExample.java
@@ -13,5 +13,23 @@ public class AutoCastingExample {
 
         // Cannot complie when the type of value is smaller than the big type
         // Like type `byte` value cannot get the `double` value.
+
+        char charValue1 = 'A';
+        char charValue2 = 1;
+        int intValue2 = charValue1 + charValue2;
+        System.out.println("유니코드=" + intValue2);
+        System.out.println("출력문자=" + (char) intValue2);
+
+        int intValue3 = 10;
+        int intValue4 = intValue3 / 4;
+        System.out.println(intValue4);
+
+        int intValue5 = 10;
+        double doubleValue1 = intValue5 / 4.0;
+        System.out.println(doubleValue1);
+
+        int intValueTest = 10;
+        byte byteValue = (byte) intValueTest;
+        System.out.println(byteValue);
     }
 }
---
+++ src/chap01/ForceCastingExample.java
@@ -0,0 +1,7 @@
+package chap01;
+
+public class ForceCastingExample {
+    public static void main(String[] args) throws Exception {
+        System.out.println("dddddd");
+    }
+}
